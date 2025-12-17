18816685f2db6d29a12dedf5bfc16ccd3bb8a80e
FilipeRPatricio <202300133@estudantes.ips.pt>
2025-12-17 17:15:53 +0000

    backup a funcionar

--- staging/backup.sql
+++ staging/backup.sql
@@ -10,19 +10,13 @@ GO
 
 
 -- Backup Completo
-
 BACKUP DATABASE AdventureWorksLegacy
-TO DISK = 'C:\Users\filip\CBDBackups\full.bak'
-WITH INIT,
-     NAME = 'AdventureWorksLegacy Full Backup',
-     STATS = 10;
-GO
-
+TO DISK = 'C:\CCBDBackups\full.bak'
+WITH INIT, COMPRESSION;
 
 -- Backup Diferencial
-
 BACKUP DATABASE AdventureWorksLegacy
-TO DISK = 'C:\Users\filip\CBDBackups\differential.bak'
+TO DISK = 'C:\CCBDBackups\differential.bak'
 WITH DIFFERENTIAL,
      INIT,
      NAME = 'AdventureWorksLegacy Differential Backup',
@@ -31,9 +25,8 @@ GO
 
 
 -- Backup de Logs
-
 BACKUP LOG AdventureWorksLegacy
-TO DISK = 'C:\Users\filip\CBDBackups\log1.trn'
+TO DISK = 'C:\CCBDBackups\log1.trn'
 WITH INIT,
      NAME = 'AdventureWorksLegacy Log Backup 01',
      STATS = 10;
@@ -41,10 +34,12 @@ GO
 
 -- cadeia de logs
 BACKUP LOG AdventureWorksLegacy
-TO DISK = 'C:\Users\filip\CBDBackups\log2.trn'
+TO DISK = 'C:\CCBDBackups\log2.trn'
 WITH INIT,
      NAME = 'AdventureWorksLegacy Log Backup 02',
      STATS = 10;
 GO
 
 
+SELECT servicename, service_account
+FROM sys.dm_server_services;
